f34a2714ad365be9c367ded9cf8923be60a6edde
RakeshKunapuli <143953416+RakeshKunapuli@users.noreply.github.com>
2024-03-06 12:02:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const app = express();
 const cors = require("cors");
 require('dotenv').config()
@@ -11,8 +12,9 @@ const orderroutes = require("../Back-End/Routes/Order.routes")
 
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use("/products", productroutes);
 app.use("/user", userRoutes);
